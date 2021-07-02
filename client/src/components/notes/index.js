f0bd5e01f290cae06e40db2a2c53b8eda7ecac1a
Dheeraj Gadwala <drgad24@gmail.com>
2021-07-02 13:36:45 +0530

    notepad area

--- client/src/components/notes/index.js
+++ client/src/components/notes/index.js
@@ -1,11 +1,28 @@
 import React from 'react';
+import './style.css';
+import Note from './note';
 
-const notes = ()=>{
+const Notes = ()=>{
     return (
-    <>
-    
-    </>
+    <div className="notesArea">
+        <Note
+        title = "note 1"
+        content = "content 1"
+        />
+        <Note
+        title = "note 2"
+        content = "content 2"
+        />
+        <Note
+        title = "note 3"
+        content = "content 3"
+        />
+        <Note
+        title = "note 4"
+        content = "content 4"
+        />
+    </div>
     );
 }
 
-export default notes;
+export default Notes;
--- client/src/components/notes/note/index.js
+++ client/src/components/notes/note/index.js
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, {useState} from 'react';
+import './style.css';
 
-const note = ()=>{
+const Note = (props)=>{
+
+    const [title, setTitle] = useState(props.title);
+    const [content, setContent] = useState(props.content);
+
     return(
-    <>
-    </>
+    <div className="note">
+        <div className="noteTitle">
+            <input type="text" className="titleInput" value={title} onChange={e=>setTitle(e.target.value)}/>
+        </div>
+        <div className="bar">
+        </div>
+        <div className="noteContent">
+            <textarea className="contentInput" value={content} onChange={e=>setContent(e.target.value)}/>
+        </div>
+    </div>
     );
 }
 
-export default note;
+export default Note;
