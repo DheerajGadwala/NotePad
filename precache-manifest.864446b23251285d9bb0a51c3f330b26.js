06d7aebd6e1fd9346601be8d97df500a24e1f584
Dheeraj Gadwala <drgad24@gmail.com>
2021-07-08 15:30:53 +0530

    Updates

---
+++ precache-manifest.864446b23251285d9bb0a51c3f330b26.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f25f10080edb7af67b8fd26dfa12129",
+    "url": "/NotePad/index.html"
+  },
+  {
+    "revision": "2b7714c6edd20e277cfd",
+    "url": "/NotePad/static/css/main.94358f3c.chunk.css"
+  },
+  {
+    "revision": "edabb48924dbd6033005",
+    "url": "/NotePad/static/js/2.d10f0da7.chunk.js"
+  },
+  {
+    "revision": "2b7714c6edd20e277cfd",
+    "url": "/NotePad/static/js/main.159259cd.chunk.js"
+  },
+  {
+    "revision": "47d85bdce6c1b66d350f",
+    "url": "/NotePad/static/js/runtime-main.efef31ee.js"
+  },
+  {
+    "revision": "9b5ba3a08c951da17a9f429dd803d4a4",
+    "url": "/NotePad/static/media/firefox.9b5ba3a0.png"
+  },
+  {
+    "revision": "08ead3be25c8a5b9446849f0c3a69774",
+    "url": "/NotePad/static/media/loading.08ead3be.gif"
+  }
+]);
